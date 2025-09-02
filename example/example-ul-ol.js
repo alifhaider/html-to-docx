9968721ca45cd4f35a1eece1105d8afb2794c637
alifhaider <alifhaider57@gmail.com>
2025-09-02 08:23:29 +0600

    feat: setup

---
+++ example/example-ul-ol.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const HTMLtoDOCX = require('../dist/html-to-docx.umd');
+
+const outputPath = './example-ul-ol.docx';
+
+const htmlString = fs.readFileSync('./example/ul-ol.html', 'utf-8');
+
+(async () => {
+  const fileBuffer = await HTMLtoDOCX(htmlString, null, {
+    table: {
+      row: { cantSplit: true },
+      addSpacingAfter: true,
+    },
+    footer: true,
+    pageNumber: true,
+    preprocessing: { skipHTMLMinify: false },
+  });
+
+  fs.writeFile(outputPath, fileBuffer, (error) => {
+    if (error) {
+      console.log('Docx file creation failed');
+      return;
+    }
+    console.log('Docx file created successfully');
+  });
+})();
